868f69275a987c55463c126cf3cc763f9f1bbf17
SheerinBanu <sbanu@allurion.com>
2023-07-24 15:24:20 +0200

    Added condition to remove semicolon in the user password b4 hashing.

--- src/main/java/com/ecommerce/bakery/Service/AuthService.java
+++ src/main/java/com/ecommerce/bakery/Service/AuthService.java
@@ -18,20 +18,26 @@ public class AuthService {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
-
     public void createNewUser(User user) {
         // Encode the user's password using the BCryptPasswordEncoder
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        // Remove any semicolons from the password before encoding
+        String password = removeSemicolons(user.getPassword());
+        user.setPassword(bCryptPasswordEncoder.encode(password));
         Role role = Role.CLIENT;
         user.setRole(role);
         userRepository.save(user);
     }
-
     public void createNewAdmin(User user) {
         // Encode the admin's password using the BCryptPasswordEncoder
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        // Remove any semicolons from the password before encoding
+        String password = removeSemicolons(user.getPassword());
+        user.setPassword(bCryptPasswordEncoder.encode(password));
         Role role = Role.ADMIN;
         user.setRole(role);
         userRepository.save(user);
+    }
+    private String removeSemicolons(String password) {
+        // Replace semicolons (;) with an empty string to remove them from the password
+        return password.replace(";", "");
     }
 }
