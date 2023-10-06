e9175ca279a9eaa66e38565ef96de4717ca6fcbe
SheerinBanu <sbanu@allurion.com>
2023-10-06 22:35:27 +0200

    removed repetitive junit dependencies and java version

---
+++ src/main/java/com/ecommerce/bakery/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.ecommerce.bakery;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(31536000); // Définit la période de mise en cache en secondes (ici, un an)
+    }
+}
